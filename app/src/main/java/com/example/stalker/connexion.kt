cc2beb6fa31025b935ab7bb8b5e5687e6432945c
fbako <bakof78@yahoo.fr>
2020-03-29 12:56:44 +0200

    Premier commit

---
+++ app/src/main/java/com/example/stalker/connexion.kt
@@ -0,0 +1,12 @@
+package com.example.stalker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class connexion : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_connexion)
+    }
+}
