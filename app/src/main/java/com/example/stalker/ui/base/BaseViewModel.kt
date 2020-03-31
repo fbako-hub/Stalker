e8d8f5b84d6ef9ba5db268e498d94623760d9961
Badini Ibrahim <sawadogo.badiniibrahim@gmail.com>
2020-04-01 00:09:28 +0200

    refacto du projet

---
+++ app/src/main/java/com/example/stalker/ui/base/BaseViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.stalker.ui.base
+
+import androidx.databinding.ObservableBoolean
+import androidx.lifecycle.ViewModel
+import java.lang.ref.WeakReference
+
+abstract class BaseViewModel<N>: ViewModel() {
+
+    private val mIsLoading = ObservableBoolean(false)
+    private var mNavigator: WeakReference<N>? = null
+
+
+    fun getIsLoading(): ObservableBoolean {
+        return mIsLoading
+    }
+
+    fun setIsLoading(isLoading: Boolean) {
+        mIsLoading.set(isLoading)
+    }
+    fun getNavigator(): N? {
+        return mNavigator!!.get()
+    }
+
+    fun setNavigator(navigator: N) {
+        this.mNavigator = WeakReference(navigator)
+    }
+
+}
--- app/src/main/java/com/example/stalker/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/stalker/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.stalker
+package com.example.stalker.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.stalker.R
 
 class MainActivity : AppCompatActivity() {
 
